8d8356b89dd3e7c15577130e76ed3072589dc1ad
3imed-jaberi <imed_jebari@hotmail.fr>
2020-06-24 12:05:10 +0200

    improve entrypoints code --js --json 👾 ..

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const faker = require('faker');
 
-
-
 /**
  * @desc generate 50 users data -> 1/2 dynamic 🥴 .. 
  */
 const generateUsers = () => {
-
   let users = [];
 
-  for(let id = 0 ; id < 50 ; id++){
-    
+  for(let id = 0 ; id < 10 ; id++){
     let firstName = faker.name.findName();
     let lastName = faker.name.lastName();
     let email = faker.internet.email();
 
-    users.push({ id, firstName, lastName, email });
-
+    users = [...users, { id, firstName, lastName, email }];
   }
 
 
@@ -24,4 +19,4 @@ const generateUsers = () => {
 }
 
 
-module.exports = generateUsers ;
+module.exports = generateUsers;
